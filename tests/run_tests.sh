a50bd66541495ddfc75d481966f0104103965f5a
MartenPostma <martenp@gmail.com>
2020-05-08 10:28:35 +0200

    added test for deps

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,3 +6,4 @@ mkdir output
 python v4_validation.py > output/v4_validation.naf 2> output/v4_validation.err
 python v4_raw_text_terms.py > output/v4_raw_text_terms.naf 2> output/v4_raw_text_terms.err
 python v4_add_entity.py > output/v4_add_entity.naf 2> output/v4_add_entity.err
+python v4_raw_text_terms_deps.py > output/v4_raw_text_terms_deps.out 2> output/v4_raw_text_terms_deps.err
